481973ec253b5f6c3a75912988692d1e77c26ec8
neheyheyhey <32345725+neheyheyhey@users.noreply.github.com>
2023-12-06 14:37:06 -0500

    adding js

---
+++ interactions.js
@@ -0,0 +1,9 @@
+new Typed(
+    '#typed',
+    {
+        strings: ["design researcher", "stragetist", "storyteller"],
+        typeSpeed: 150,
+        loop: true,
+        loopCount: Infinity,      
+    }
+)
